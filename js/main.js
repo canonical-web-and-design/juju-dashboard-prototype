060a0578c23057d78a56a137bf73f05ac9e0c7a6
Claudio Gomboli <claudio.gomboli@canonical.com>
2019-09-18 17:34:24 +0200

    Shell
    Added shell as a panel

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 $(document).ready( function () {
+  //start
   $(".sidebar.collapsed").slideDown(0);
   $(".sidebar.active").slideUp(0);
+  $(".shell").slideDown(0);
+  $(".collapsedshell").slideUp(0);
 
+  //sidebar interaction
   $(".sidebar").mouseenter(function() {
     $(".sidebar.collapsed").slideUp('fast');
     $(".sidebar.active").slideDown('slow');
   }).mouseleave(function() {
     $(".sidebar.collapsed").slideDown('fast');
     $(".sidebar.active").slideUp(0);
+  });
+
+  //shell interaction
+  $(document).on('click', '.chevron', function(){
+    $(".shell").slideToggle('fast');
+    $(".collapsedshell").slideToggle('fast');
   });
 
 });
