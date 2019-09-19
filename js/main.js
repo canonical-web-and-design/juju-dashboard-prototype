eac140154ff565ed158a18f4b1fb47cc9e159ebb
Claudio Gomboli <claudio.gomboli@canonical.com>
2019-09-19 17:51:21 +0200

    Updated content
    Updated content for models details

--- js/main.js
+++ js/main.js
@@ -3,17 +3,14 @@ $(document).ready( function () {
   $(".shell").slideDown(0);
   $(".collapsedshell").slideUp(0);
   $('.submenu').hide();
-  $('.thirdlevel').hide();
 
   //sidebar interaction
   $(".sidebar").mouseenter(function() {
     $(this).css('width','260px');
-    $('.submenu').slideToggle();
-    $('.thirdlevel').slideToggle();
+    $('.submenu').slideToggle(500);
   }).mouseleave(function() {
     $(this).css('width','46px');
-    $('.submenu').slideToggle();
-    $('.thirdlevel').slideToggle();
+    $('.submenu').slideToggle(100);
   });
 
   //shell interaction
